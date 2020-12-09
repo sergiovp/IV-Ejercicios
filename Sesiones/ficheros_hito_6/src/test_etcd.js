275a5b0a797e7e9e15f8253b77128dba27d03ed7
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-12-09 18:39:07 +0100

    Añadido código de ejemplo para el ejercicio1

--- Sesiones/ficheros_hito_6/src/test_etcd.js
+++ Sesiones/ficheros_hito_6/src/test_etcd.js
@@ -2,3 +2,14 @@
     Fichero para hacer un pequeño ejemplo de uso de etcd, relativo al hito 6.
 */
 
+const { Etcd3 } = require('etcd3');
+const client = new Etcd3();
+
+(async () => {
+    const clave = await client.get('clave').string();
+    
+    console.log('La clave introducida es: ', clave);
+  
+    await client.delete().all();
+
+})();
